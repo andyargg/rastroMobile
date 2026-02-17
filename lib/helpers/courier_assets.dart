9abd2f4bf37272b9441703e2eab17858e9616e9b
andyarggg <andyarguiwork@gmail.com>
2026-02-17 19:58:55 -0300

    Remove OCA from UI courier options

--- lib/helpers/courier_assets.dart
+++ lib/helpers/courier_assets.dart
@@ -1,5 +1,4 @@
 const Map<String, String> courierLogos = {
   'Correo Argentino': 'assets/couriers/correo_argentino.png',
   'Andreani': 'assets/couriers/andreani.jpg',
-  'OCA': 'assets/couriers/oca.png',
 };
